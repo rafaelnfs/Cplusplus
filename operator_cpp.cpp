b0d19530c884ca0de51ecba008fb992b1aaf9a3f
Rafaelnfs <rafaelnfs261189@gmail.com>
2016-08-23 11:39:35 -0600

    Operator_Cpp
    Aca vemos un breve ejemplo de como utilizar la palabra reservada operator que es para sobrecargar un operador en una clase con lo que podemos , sumar, restar, multiplicar y dividir, siempre con una clase.

---
+++ operator_cpp.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+class numeros {
+	int n;
+public:
+	void set(int a) { n = a;}
+	void show(){  cout<<n<<endl; }
+    numeros operator +(numeros op); // implementacion de la clase
+    
+	
+};
+
+
+// funcion que sobrecarga operador +
+ numeros numeros::operator +(numeros op){
+	numeros temp;
+	temp.n = n + op.n; // retenemos la variable 
+	return temp;
+}
+
+
+
+
+int main(int argc, char *argv[]) {
+	
+numeros a,b,c;
+
+
+a.set(23);
+a.show();
+
+b.set(1);
+b.show();
+
+c = a +b; 
+c.show();
+	
+	
+	
+	return 0;
+}
+
